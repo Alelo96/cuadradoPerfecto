1a067f90253b2fe3d64128225d142808c5373c45
Alex Lopez <alexlopez@inspladelestany.cat>
2022-01-24 17:03:07 +0100

    Comentat el que s'ha de fer

--- src/Cuadrat.java
+++
@@ -1,3 +0,0 @@
-public class Cuadrat {
-    //Multiplicar fins que l'arrel quadrada del resultat multiplicat no dongui decimal
-}
---
+++ src/Quadrat.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Quadrat {
+    //Multiplicar fins que l'arrel quadrada del resultat multiplicat no dongui decimal
+
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+
+}
